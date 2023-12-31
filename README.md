480634b8411937916fa58b0ebe8bf9a6d0c4de34
rekwet <31699450+rekwet@users.noreply.github.com>
2023-12-31 08:27:09 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ai
+
+Status: Work in progress
--- code/1.py
+++ code/1.py
@@ -1,11 +1,17 @@
 import os
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings import GPT4AllEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.llms import gpt4all
 
 # Load the document, split it into chunks, embed each chunk and load it into the vector store.
-loader = PyPDFLoader("./data/telkom.pdf")
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+loader = PyPDFLoader("./data/machinelearning-lecture01.pdf")
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 documents = text_splitter.split_documents(loader)
-db = FAISS.from_documents(documents, GPT4AllEmbeddings())
+embeddings = GPT4AllEmbeddings()
+db = FAISS.from_documents(documents, embeddings)
+
+qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=db.as_retriever())
+
